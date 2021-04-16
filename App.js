6da0984b945016db7d07c2e25e8c18fb2179d284
= <=>
2021-04-16 03:59:11 -0500

    2nd

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import { Button, Text, View, Alert, Image } from 'react-native';
+import { Button, Text, View, Image } from 'react-native';
+import { NavigationContainer as Nc } from '@react-navigation/native';
+import { createStackNavigator as Ns } from '@react-navigation/stack';
 
 const styles = {
+  mainView:{
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center'
+  },
   mainTitle:{
     fontSize:20,
     fontWeigt:'bold'
@@ -21,10 +28,12 @@ const styles = {
   }
 }
 
-export default function App() {
+const Stack = Ns();
+
+function main({ navigation }){
   return (
     <View style={{flex:1}}>
-      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <View style={styles.mainView}>
         <Text style={styles.mainTitle}>Instituto Tecnológico de Colima</Text>
         <Image style={styles.logoImage} source={require('./assets/itcolima.jpg')}></Image>
         <Text>Ingeniería en informática</Text>
@@ -32,7 +41,35 @@ export default function App() {
         <Text style={styles.txtnombre}>Juan Luis Vázquez Hernández</Text>
         <Text>Marzo-Junio 2021</Text>
       </View>
-      <Button style={styles.ctaButton} title="Ingresar" onPress={() => {Alert.alert("No me estés picoteando")}}/>
+      <Button style={styles.ctaButton} title="Acerca" onPress={()=>{navigation.navigate('About')}}/>
     </View>
   );
 }
+
+function aboutUs({ navigation }){
+  return(
+    <View style={{flex:1}}>
+      <View style={styles.mainView}>
+        <Text style={styles.mainTitle}>Acerca</Text>
+      </View>
+    <Button style={styles.ctaButton} title="Regresar" onPress={()=>navigation.goBack()} />
+    <Button style={styles.ctaButton} title="Regresar a la pagina principal" onPress={()=>navigation.navigate('Main')} />
+    <Button style={styles.ctaButton} title="Regresar acerca" onPress={()=>navigation.navigate('About')} />
+    <Button style={styles.ctaButton} title="Forzar regresar" onPress={()=>navigation.push('About')} />
+    <Button style={styles.ctaButton} title="Regresar al inicio" onPress={()=>navigation.popToTop()} />
+  </View>
+  );
+}
+
+function App() {
+  return(
+    <Nc>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={main} />
+        <Stack.Screen name="About" component={aboutUs} />
+      </Stack.Navigator>
+    </Nc>
+  );
+}
+
+export default App;
